c2838896a5861e6a437876b92afcd3b0fffea134
DENRIG777 <62824680+DENRIG777@users.noreply.github.com>
2021-11-12 07:18:21 -0600

    added content

--- js/site.js
+++ js/site.js
@@ -1,4 +1,23 @@
-// Call Hello World
-function HelloWorld() {
-    alert("hello World");
+// get the values from the page
+// starts or controller function
+function getValues(){
+    // get values from the page
+    let startValue = document.getElementById("startValue").getValues;
+    let endValue = document.getElementById("endValue").getValues;
+    let  = document.getElementById("startValue").getValues;
+    // we call generateNumbers
+
+    // we call displayNumbers
+}
+
+// generate numbers from startValue to the endValue
+// logic function(s)
+function generateNumbers(){
+
+}
+
+// display the numbers and mark even numbers bold
+// display or view functions
+function displayNumbers(){
+
 }
